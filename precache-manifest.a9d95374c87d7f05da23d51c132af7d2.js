2bcb4e09a79b4d2413542f5cb5ed6ce5a60c8a42
rizwan-ak <rizwan.ak@outlook.com>
2020-07-19 21:55:35 +0500

    Updates

---
+++ precache-manifest.a9d95374c87d7f05da23d51c132af7d2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c61ec97aa9b3ad90e3091ec8a9d7b695",
+    "url": "/campaignsEditor/index.html"
+  },
+  {
+    "revision": "c40129521a775d1f099b",
+    "url": "/campaignsEditor/static/css/main.86d8dbcd.chunk.css"
+  },
+  {
+    "revision": "801bad2bfa78dfbafa0f",
+    "url": "/campaignsEditor/static/js/2.ea5d5cdf.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/campaignsEditor/static/js/2.ea5d5cdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c40129521a775d1f099b",
+    "url": "/campaignsEditor/static/js/main.451b51ca.chunk.js"
+  },
+  {
+    "revision": "02a7fb3a21ab4aaabdc6",
+    "url": "/campaignsEditor/static/js/runtime-main.3183b3da.js"
+  }
+]);
